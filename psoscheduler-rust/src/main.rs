6a8fb96ed448c6fffd6a2ece3c93cf80e5a18c16
David Gerard <dgredsox@gmail.com>
2023-11-20 13:57:16 -0500

    fix Task simulation

--- psoscheduler-rust/src/main.rs
+++ psoscheduler-rust/src/main.rs
@@ -1,11 +1,16 @@
+use crate::simulation::task;
+
 mod schedule {
     mod scheduler;
 }
 
-mod utils {
-    pub(crate) mod utilities;
+mod simulation {
+    pub(crate) mod task;
 }
 
 fn main() {
     println!("Hello, world!");
+    let t = task::Task::new(500);
+    let t2 = task::Task::new(600);
+    println!("{} {}", t, t2);
 }
--- psoscheduler-rust/src/simulation/task.rs
+++ psoscheduler-rust/src/simulation/task.rs
@@ -1,18 +1,48 @@
+use core::fmt;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 static TASK_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 pub struct Task {
   id: usize,
-
+  workload_id: usize,
+  millions_of_instructions: i32
 }
 
 impl Task {
-  pub fn new() -> Task {
-    Task { TASK_ID_COUNTER.fetch_add(1, Ordering::Relaxed); }
+  pub fn new(millions_of_instructions: i32) -> Task {
+    Task { 
+      id: TASK_ID_COUNTER.fetch_add(1, Ordering::Relaxed),
+      workload_id: 0,
+      millions_of_instructions: millions_of_instructions
+    }
   }
 
-  fn get_id() -> usize {
-    id;
+  fn get_id(&self) -> usize {
+    return self.id;
   }
+
+  fn get_millions_of_instructions(&self) -> i32 {
+    return self.millions_of_instructions;
+  }
+
+  fn get_workload_id(&self) -> usize {
+    return self.workload_id;
+  }
+
+  fn set_workload_id(&mut self, workload_id: usize) {
+    self.workload_id = workload_id;
+  }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      let mut output: String = String::new();
+      output.push_str("(TSK");
+      output.push_str(&self.get_id().to_string());
+      output.push_str(": ");
+      output.push_str(&self.millions_of_instructions.to_string());
+      output.push_str(" mi)");
+      return write!(f, "{}", output);
+    }
 }
