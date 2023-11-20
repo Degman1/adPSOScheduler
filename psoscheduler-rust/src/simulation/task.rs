0c07306285207dc08e7ab854a0aff7a338b0703c
David Gerard <dgredsox@gmail.com>
2023-11-19 23:52:02 -0500

    add id generation to task struct

--- psoscheduler-rust/src/simulation/task.rs
+++ psoscheduler-rust/src/simulation/task.rs
@@ -1,13 +1,18 @@
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 static TASK_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 pub struct Task {
+  id: usize,
 
 }
 
 impl Task {
+  pub fn new() -> Task {
+    Task { TASK_ID_COUNTER.fetch_add(1, Ordering::Relaxed); }
+  }
+
   fn get_id() -> usize {
-    TASK_ID_COUNTER.fetch_add(1, Ordering::Relaxed)
+    id;
   }
 }
